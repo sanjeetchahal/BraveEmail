8b394471762fd0cb5bf8807eb3d12456a4056412
santosh <webfort.santoshgautam@gmail.com>
2023-11-14 19:02:58 +0530

    remove username and password on config.php file

--- includes/config.php
+++ includes/config.php
@@ -3,14 +3,14 @@
 return array (
   'redirect_url' => 'http://localhost/wordpress-saas/wp-admin/admin.php?page=brave-email-smtp&provider=google&tabview=usage',
   'active_provider' => 'google',
-  'client_secret' => 'google/uploads/client_secret_741383489373-d7p16oobctkc986dqitltst0b3j2rtm2.apps.googleusercontent.com (2).json',
+  'client_secret' => 'google/uploads/client_secret_579282992989-1rskmfavtkqntsp07kidicgv8s16qvf7.apps.googleusercontent.com (1).json',
   'api_status' => true,
-  'smtp_settings' => 
+  'smtp_settings' =>
   array (
     'host' => 'smtpout.secureserver.net',
     'encryptionType' => 'tls',
     'port' => '587',
-    'username' => 'support@bulkphonelookup.com',
-    'password' => 'A3gzr9&80',
+    'username' => '',
+    'password' => '',
   ),
 );
